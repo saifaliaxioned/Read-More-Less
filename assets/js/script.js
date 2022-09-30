323e7c252022605ad72d1d4a5e72ee16ef7201ea
saif <saifalis@axioned.com>
2022-09-30 16:14:07 +0530

    show hide function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-/* Author: 
+// Read More/Less function
+var serviceList = document.querySelectorAll('.services-list');
+var description = document.querySelectorAll('.hidden-desc');
+var dots = document.querySelectorAll('.dots');
 
-*/
+var controlBtn = document.querySelectorAll('.toggle-btn'); 
+controlBtn.forEach(function (btn,index) {
+  btn.addEventListener('click',function () {
+    serviceList[index].classList.toggle('active-list');
+    description[index].classList.toggle('hide-content');
+    dots[index].classList.toggle('hide-content');
+    if (btn.innerHTML === 'Read Less') {
+      btn.innerHTML = 'Read More';
+    } else {
+      btn.innerHTML = 'Read Less';
+    }
+  })
+})
 
 
 
